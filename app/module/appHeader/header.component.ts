def6443ee7a7904025f18904ab81c72c6ce628f4
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-16 20:00:39 +0530

    Show/Hide Menu Button and search box

--- app/module/appHeader/header.component.ts
+++ app/module/appHeader/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {CommonAppService} from '../../sharedService/app.common-service';
 
 @Component({
     selector: 'app-header',
@@ -11,8 +12,12 @@ import {Location} from '@angular/common';
 export class AppHeaderComponent implements OnInit{
 
     selectedPage : string;
+    private loggedIn : boolean = false;
+    private menuDisplay : boolean = false;
 
-    constructor(private router : Router, private location : Location){}
+    constructor(private router : Router,
+                private location : Location,
+                private commonService : CommonAppService){}
 
     navigate(pageName: string) : void {
         let navigationPage = "/"+pageName;
@@ -21,5 +26,16 @@ export class AppHeaderComponent implements OnInit{
 
     ngOnInit() : void {
         this.selectedPage = this.location.path().split("/")[1];
+        var userCookie = this.commonService.getCookies("userId");
+        if(userCookie !== ''){
+            this.loggedIn = true;
+        }else {
+            this.loggedIn = false;
+        }
+    }
+
+    toggleMenu() : void {
+        if(!this.menuDisplay) this.menuDisplay = true;
+        else this.menuDisplay = false;
     }
 }
