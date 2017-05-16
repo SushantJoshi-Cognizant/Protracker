d3e131934d7e08734178c7496c56e6ef93e40da7
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-16 20:03:37 +0530

    Login functionality

--- app/module/login/login.module.ts
+++ app/module/login/login.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-
+import {ToasterModule, ToasterService} from 'angular2-toaster';
 import {LoginComponent} from '../login/login.component'
+import {LoginProvider} from '../../providers/login/login.provider';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
-    imports: [RouterModule],
+    imports: [RouterModule, ToasterModule, FormsModule, CommonModule],
     declarations: [LoginComponent],
-    providers: [],
+    providers: [LoginProvider],
     bootstrap: [LoginComponent]
 })
 
