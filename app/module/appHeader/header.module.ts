c9cadf462f2430b7b6d99f5398e5efca70d21a54
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-16 20:01:09 +0530

    Adding Common Module for ngIf

--- app/module/appHeader/header.module.ts
+++ app/module/appHeader/header.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-
+import {CommonModule} from '@angular/common';
 import {AppHeaderComponent} from './header.component';
 
 
 @NgModule({
-    imports:        [],
+    imports:        [CommonModule],
     declarations:   [AppHeaderComponent],
     providers:      [],
     bootstrap:      [AppHeaderComponent],
